ea99a2cbe5e6bd075f69b8003cc1b93627c6c047
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-07-21 23:52:43 +0600

    testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,32 @@
+// import "dotenv/config";
+// import http from "http";
+// import app from "./app";
+// import { connectDB } from "./db";
+
+// const server = http.createServer(app);
+
+// const port = process.env.PORT || 3000;
+
+// const main = async (): Promise<void> => {
+//   try {
+//     await connectDB();
+//     server.listen(port, () => {
+//       console.log(`Server running on port ${port}`);
+//     });
+//   } catch (error) {
+//     console.log("Database Error");
+//     console.log(error);
+//   }
+// };
+
+// main();
+
 import "dotenv/config";
 import http from "http";
 import app from "./app";
 import { connectDB } from "./db";
 
 const server = http.createServer(app);
-
 const port = process.env.PORT || 3000;
 
 const main = async (): Promise<void> => {
